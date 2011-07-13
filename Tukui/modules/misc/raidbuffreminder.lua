f63e35795d09864b16eb103ee9249ea1055bb5f6
liquidbase <Christian Hann@.(none)>
2011-07-13 13:03:35 +0200

    Added mouseover for RaidbuffReminder

--- Tukui/modules/misc/raidbuffreminder.lua
+++ Tukui/modules/misc/raidbuffreminder.lua
@@ -293,6 +293,8 @@ raidbuff_reminder:RegisterEvent("UPDATE_BONUS_ACTIONBAR")
 raidbuff_reminder:RegisterEvent("CHARACTER_POINTS_CHANGED")
 raidbuff_reminder:RegisterEvent("ZONE_CHANGED_NEW_AREA")
 raidbuff_reminder:SetScript("OnEvent", OnAuraChange)
+raidbuff_reminder:SetScript("OnEnter", function(self) self:SetAlpha(1) end)
+raidbuff_reminder:SetScript("OnLeave", function(self) self:SetAlpha(0) end)
 
 --Function to create buttons
 local function CreateButton(name, relativeTo, firstbutton)
