35d2ecc4cad047b7d729da59ae3d77c436d9c08c
liquidbase <Christian Hann@.(none)>
2011-07-22 10:28:52 +0200

    Adjustments for bar2 - bar4.lua

--- Tukui/modules/actionbars/Bar3.lua
+++ Tukui/modules/actionbars/Bar3.lua
@@ -5,30 +5,7 @@ if not C["actionbar"].enable == true then return end
 -- setup MultiBarLeft as bar #3 
 ---------------------------------------------------------------------------
 
-if C["actionbar"].layout == 1 then
-	local bar = TukuiBar2
-	if C["actionbar"].swapbar1and3 then
-		bar = TukuiBar1
-	end
-	MultiBarBottomRight:SetParent(bar)
-
-	for i= 1, 12 do
-		local b = _G["MultiBarBottomRightButton"..i]
-		local b2 = _G["MultiBarBottomRightButton"..i-1]
-		b:SetSize(T.buttonsize, T.buttonsize)
-		b:ClearAllPoints()
-		b:SetFrameStrata("MEDIUM")
-		b:SetFrameLevel(15)
-	
-		if i == 1 then
-			b:SetPoint("BOTTOMLEFT", bar, T.buttonspacing, T.buttonspacing)
-		else
-			b:SetPoint("LEFT", b2, "RIGHT", T.buttonspacing, 0)
-		end
-	end
-end
-
-if C["actionbar"].layout == 2 then
+if C["actionbar"].layout ~= 1 then
 	local barL = TukuiBar3Left
 	local barR = TukuiBar3Right
 	barR:SetParent(barL)
@@ -52,6 +29,27 @@ if C["actionbar"].layout == 2 then
 			b:SetPoint ("BOTTOMLEFT", barR, T.buttonspacing, T.buttonspacing)
 		else
 			b:SetPoint ("LEFT", b2, "RIGHT", T.buttonspacing, 0)
+		end
+	end
+else
+	local bar = TukuiBar2
+	if C["actionbar"].swapbar1and3 then
+		bar = TukuiBar1
+	end
+	MultiBarBottomRight:SetParent(bar)
+
+	for i= 1, 12 do
+		local b = _G["MultiBarBottomRightButton"..i]
+		local b2 = _G["MultiBarBottomRightButton"..i-1]
+		b:SetSize(T.buttonsize, T.buttonsize)
+		b:ClearAllPoints()
+		b:SetFrameStrata("MEDIUM")
+		b:SetFrameLevel(15)
+	
+		if i == 1 then
+			b:SetPoint("BOTTOMLEFT", bar, T.buttonspacing, T.buttonspacing)
+		else
+			b:SetPoint("LEFT", b2, "RIGHT", T.buttonspacing, 0)
 		end
 	end
 end
