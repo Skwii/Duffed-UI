c268b1b7b1b9779339563d8aa2ddd21bb629fa31
liquidbase <Christian Hann@.(none)>
2011-07-24 10:23:01 +0200

    forget an ","

--- Tukui/medias/medias.lua
+++ Tukui/medias/medias.lua
@@ -5,7 +5,7 @@ C["media"] = {
 	["font"] = [=[Interface\Addons\Tukui\medias\fonts\normal_font.ttf]=], 					-- general font of tukui
 	["uffont"] = [[Interface\AddOns\Tukui\medias\fonts\uf_font.ttf]], 						-- general font of unitframes
 	["dmgfont"] = [[Interface\AddOns\Tukui\medias\fonts\combat_font.ttf]], 					-- general font of dmg / sct
-	["outline"] = "THINOUTLINE"																-- general outline
+	["outline"] = "THINOUTLINE",															-- general outline
 	
 	-- fonts (RUSSIAN)
 	["ru_font"] = [=[Interface\Addons\Tukui\medias\fonts\normal_font.ttf]=], 				-- general font of tukui
