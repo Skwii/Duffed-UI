940f8c028ac10e98b9e2e71266e040b08c439e0b
liquidbase <liquid.base.x@googlemail.com>
2011-09-09 18:31:55 +0200

    Patch for Skadaskin

--- Tukui/modules/skins/addons/Skada.lua
+++ Tukui/modules/skins/addons/Skada.lua
@@ -1,12 +1,40 @@
-local T, C, L = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
+﻿local T, C, L = unpack(select(2, ...)) -- Import Functions/Constants, Config, Locales
+
 
 if not IsAddOnLoaded("Skada") or not C.skins.Skada then return end
+local Skada = Skada
 
 local Skada = Skada
 local barSpacing = T.Scale(1, 1)
 local borderWidth = T.Scale(2, 2)
 
 local barmod = Skada.displays["bar"]
+
+-- Used to strip unecessary options from the in-game config
+local function StripOptions(options)
+	options.baroptions.args.barspacing = nil
+	options.titleoptions.args.texture = nil
+	options.titleoptions.args.bordertexture = nil
+	options.titleoptions.args.thickness = nil
+	options.titleoptions.args.margin = nil
+	options.titleoptions.args.color = nil
+	options.windowoptions = nil
+	options.baroptions.args.barfont = nil
+	options.titleoptions.args.font = nil
+end
+
+local barmod = Skada.displays["bar"]
+barmod.AddDisplayOptions_ = barmod.AddDisplayOptions
+barmod.AddDisplayOptions = function(self, win, options)
+	self:AddDisplayOptions_(win, options)
+	StripOptions(options)
+end
+
+for k, options in pairs(Skada.options.args.windows.args) do
+	if options.type == "group" then
+		StripOptions(options.args)
+	end
+end
 
 local titleBG = {
 	bgFile = C["media"].normTex,
@@ -26,16 +54,27 @@ barmod.ApplySettings = function(self, win)
 
 	skada:SetTexture(C["media"].normTex)
 	skada:SetSpacing(barSpacing)
-	skada:SetFont(C["media"].font, C["general"].fontscale)
+	skada:SetFont(C.media.font, C["datatext"].fontsize, "THINOUTLINE")
 	skada:SetFrameLevel(5)
+	
+	local titlefont = CreateFont("TitleFont"..win.db.name)
+	titlefont:SetFont(C.media.font, C["datatext"].fontsize, "THINOUTLINE")
+	win.bargroup.button:SetNormalFontObject(titlefont)
+
+	local color = win.db.title.color
+	win.bargroup.button:SetBackdropColor(unpack(C["media"].bordercolor))
 
 	skada:SetBackdrop(nil)
 	if not skada.backdrop then
-		skada:CreateBackdrop('Default')
+		skada:CreateBackdrop("Default")
 	end
 	skada.backdrop:ClearAllPoints()
-	skada.backdrop:Point('TOPLEFT', win.bargroup.button or win.bargroup, 'TOPLEFT', -2, 2)
-	skada.backdrop:Point('BOTTOMRIGHT', win.bargroup, 'BOTTOMRIGHT', 2, -4)
+	if win.db.enabletitle then
+		skada.backdrop:Point('TOPLEFT', win.bargroup.button, 'TOPLEFT', -2, 2)
+	else
+		skada.backdrop:Point('TOPLEFT', win.bargroup, 'TOPLEFT', -2, 2)
+	end
+	skada.backdrop:Point('BOTTOMRIGHT', win.bargroup, 'BOTTOMRIGHT', 2, -2)
 end
 
 -- Update pre-existing displays
